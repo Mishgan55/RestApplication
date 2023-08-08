be6b5cd1db8d10bcf5c950fcbb629eff11835d0f
mishgan55 <vorota.mogilev@gmail.com>
2023-08-08 16:33:54 +0300

    Initial commit

---
+++ FirstRestApplication/src/test/java/khorsun/spring/FirstRestApplication/FirstRestApplicationTests.java
@@ -0,0 +1,13 @@
+package khorsun.spring.FirstRestApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
